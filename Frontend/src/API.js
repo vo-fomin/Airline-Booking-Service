37fa25325965ac3985186ee22eb71706fa0a6bc3
Volodymyr <w260698@gmail.com>
2017-12-10 21:36:06 +0200

    Merge initial

--- Frontend/src/API.js
+++ Frontend/src/API.js
@@ -13,7 +13,7 @@ function backendGet(url, callback) {
     })
 }
 
-/*function backendPost(url, data, callback) {
+function backendPost(url, data, callback) {
     $.ajax({
         url: API_URL + url,
         type: 'POST',
@@ -26,12 +26,20 @@ function backendGet(url, callback) {
             callback(new Error("Ajax Failed"));
         }
     })
-}*/
+}
 
 exports.getFlights = function(callback) {
     backendGet("/api/get-flights/", callback);
 };
 
-/*exports.createOrder = function(order_info, callback) {
+exports.createOrder = function(order_info, callback) {
     backendPost("/api/create-order/", order_info, callback);
-};*/
+};
+
+exports.sendMail=function(data, callback){
+    backendPost("/api/send-mail/", data, callback);
+};
+
+exports.getClientData=function(callback){
+    backendGet("/api/get-client-data/", callback);
+};
--- Frontend/src/googleMaps.js
+++ Frontend/src/googleMaps.js
@@ -2,17 +2,33 @@ var map;
 
 function initialize() {
     var mapProp = {
-        center: new google.maps.LatLng(50.464379, 30.519131),
-        zoom: 14
+        center: new google.maps.LatLng(50.454379, 30.519131),
+        zoom: 11
     };
     var html_element = document.getElementById("googleMap");
     map = new google.maps.Map(html_element, mapProp);
-    var point = new google.maps.LatLng(50.464379, 30.519131);
-    var marker = new google.maps.Marker({
-        position: point,
-//map - це змінна карти створена за допомогою new google.maps.Map(...)
+    var arrayPoints=[
+        new google.maps.LatLng(50.444379, 30.519131),
+        new google.maps.LatLng(50.504379, 30.499931),
+        new google.maps.LatLng(50.402379, 30.635131)
+    ];
+    var arrayMarkers=[
+        new google.maps.Marker({
+        position: arrayPoints[0],
+        //map - це змінна карти створена за допомогою new google.maps.Map(...)
         map: map
-    });
+        }),
+        new google.maps.Marker({
+            position: arrayPoints[1],
+            //map - це змінна карти створена за допомогою new google.maps.Map(...)
+            map: map
+        }),
+        new google.maps.Marker({
+            position: arrayPoints[2],
+            //map - це змінна карти створена за допомогою new google.maps.Map(...)
+            map: map
+        })
+    ];
 //Карта створена і показана
 }
 
